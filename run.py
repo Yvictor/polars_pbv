2c04d2e50afdee9328371bd568c670ba4ae3c228
yvictor <yvictor3141@gmail.com>
2024-05-29 23:03:56 +0800

    feat: run

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 import polars as pl
-from polars_pbv import pig_latinnify
+import polars_pbv as pl_pbv
 
+price_col = [100, 101, 102, 103, 104, 105, 106, ]#107, 108, 109]
+volume_col = [200, 220, 250, 240, 260, 300, 280, ]# 270, 310, 330]
+window_size = 6#5.0
+bins = 3
 df = pl.DataFrame({
-    'english': ['this', 'is', 'not', 'pig', 'latin'],
+    'price': price_col,
+    'volume': volume_col
 })
-result = df.with_columns(pig_latin = pig_latinnify('english'))
-print(result)
+df_result = df.with_columns(
+    pl_pbv.pbv("price", "volume", window_size=window_size, bins=bins, center=True, round=2).alias("pbv"),
+    pl_pbv.pbv_pct("price", "volume", window_size=window_size, bins=bins, center=True, round=4).alias("pbv_pct"),
+    pl_pbv.pbv_topn_vp("price", "volume", window_size=window_size, bins=bins, n=2, center=True, round=2).alias("pbv_topn_vp"),
+    pl_pbv.pbv_topn_vp("price", "volume", window_size=window_size, bins=bins, n=1, center=True, round=2).list.get(0).alias("pbv_top1_vp"),
+    pl_pbv.pbv_topn_v("price", "volume", window_size=window_size, bins=bins, n=2, center=True, round=2).alias("pbv_topn_v"),
+)
+print(df_result)
 
